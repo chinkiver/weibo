bf8a9c0406f193c080906c6b5fd1102ce6414489
vagrant <vagrant@homestead>
2022-01-19 15:24:11 +0000

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,12 +2,21 @@
 <html lang="{{ config('app.locale') }}">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title')</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  @yield('content')
+
+  @include('layouts._header')
+
+  <div class="container">
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
+  </div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,20 @@
 @section('title', '首页')
 
 @section('content')
-  <h1>首页</h1>
+
+  <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5" style="background-color: #ced4da">
+      <h1 class="display-5 fw-bold">Hello Laravel</h1>
+      <p class="col-md-8 fs-4">
+        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+      </p>
+      <p class="col-md-8 fs-4">
+        一切，将从这里开始。
+      </p>
+      <p class="col-md-8 fs-4">
+        <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      </p>
+    </div>
+  </div>
+
 @stop
