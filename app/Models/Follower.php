ebec88c2b0229c2a64877de5c5e17b6bd1dfec5b
陈勇 <yong.chen@bjrxtd.com>
2022-02-11 13:22:36 +0800

    11.2

---
+++ app/Models/Follower.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Follower extends Model
+{
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -74,4 +74,64 @@ class User extends Authenticatable
     {
         return $this->statuses()->orderByDesc('created_at');
     }
+
+    /**
+     * 关注某一个用户
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     * 哪些人关注了某一用户
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    /**
+     * 关注用户
+     *
+     * @param $userIds
+     */
+    public function follow($userIds)
+    {
+        if ( ! is_array($userIds)) {
+            $userIds = compact('userIds');
+        }
+
+        $this->followers()->sync($userIds, false);
+    }
+
+    /**
+     * 取消关注
+     *
+     * @param $userIds
+     */
+    public function unfollow($userIds)
+    {
+        if ( ! is_array($userIds)) {
+            $userIds = compact('userIds');
+        }
+
+        $this->followers()->detach($userIds);
+    }
+
+    /**
+     * 该用户是否已经关注了某个用户
+     *
+     * @param int $userId 被关注人
+     *
+     * @return mixed
+     */
+    public function isFollowing($userId)
+    {
+        return $this->followings->contains($userId);
+    }
 }
